d3c0a0fdc4425acd9ac025048aedd66a673b8eb4
Aniket Kumar <ani1998ket@gmail.com>
2021-02-04 23:51:48 +0530

    Added basic MMU functionality

--- src/MMU.cpp
+++ src/MMU.cpp
@@ -1,3 +1,17 @@
 #include "MMU.h"
 
 MMU::MMU(){}
+
+Byte MMU::read( Pointer address )
+{
+    if( address >= 0xc000 && address <= 0xdfff ){
+        return internal_ram[address];
+    }
+}
+
+void MMU::write( Pointer address, Byte value)
+{
+    if( address >= 0xc000 && address <= 0xdfff ){
+        internal_ram[address] = value;
+    }
+}
--- src/MMU.h
+++ src/MMU.h
@@ -1,7 +1,20 @@
 #pragma once
 
+#include "utils.h"
+
+static const int INTERNAL_RAM_SIZE = 8192;
+
 class MMU{
+
 public:
+
     MMU();
+
+    Byte read( Pointer address );
+    void write( Pointer address, Byte value);
+
 private:
+
+    Byte internal_ram[INTERNAL_RAM_SIZE] = {0};
+
 };
