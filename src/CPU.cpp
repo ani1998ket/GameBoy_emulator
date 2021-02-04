b9835c6d70bba0b54e89a668a83f2731d0bf9849
Aniket Kumar <ani1998ket@gmail.com>
2021-02-04 23:38:37 +0530

    Added registers and basic functions

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -1,3 +1,32 @@
 #include "CPU.h"
 
-CPU::CPU(){}
+#define BC combine(B, C);
+#define DE combine(D, E);
+#define HL combine(H, L);
+
+CPU::CPU()
+{
+
+}
+
+void CPU::connect(MMU* p_mmu)
+{
+    this->p_mmu = p_mmu;
+}
+
+void CPU::reset()
+{
+    A = 0;
+    B = 0;
+    C = 0; 
+    D = 0; 
+    E = 0;
+    F.Z = F.N = F.H = F.C = false;
+    SP = 0;
+    PC = 0;
+}
+
+Pointer combine(Register high, Register low)
+{
+    Pointer result = (high<<8) + low;
+}
--- src/CPU.h
+++ src/CPU.h
@@ -1,7 +1,29 @@
 #pragma once
 
+#include "utils.h"
+#include "MMU.h"
+
+struct Flag{
+    bool Z, N, H, C;
+};
+
 class CPU{
 public:
     CPU();
+    void connect(MMU* p_mmu);
+    void reset();
+
 private:
+
+    Register A, B, C, D, E, H, L;
+    Pointer SP, PC;
+    Flag F;
+    MMU* p_mmu;
+
+    Byte opcode;
+    Byte arg1, arg2;
+
+private:
+
+    Pointer combine( Register high, Register low);
 };
