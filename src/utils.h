1e5f77ac65910d2822c69683f2071869b5a5f505
Aniket Kumar <ani1998ket@gmail.com>
2021-02-11 17:11:26 +0530

    Added helper functions

--- src/utils.h
+++ src/utils.h
@@ -4,3 +4,17 @@ using Byte = uint8_t;
 using Pointer = uint16_t;
 
 using Register = uint8_t; 
+
+inline Byte low_nibble ( Byte value ){
+    return (Byte)( 0x0f & value ); 
+}
+inline Byte high_nibble(Byte value ){
+    return (Byte)( value >> 4 );
+}
+
+inline Byte low_byte( Pointer value ){
+    return (Byte)( 0xff & value ); 
+}
+inline Byte high_byte( Pointer value ){
+    return (Byte)( value >> 8 );
+}
